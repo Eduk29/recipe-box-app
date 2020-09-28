2c54077d8cb426a92ae8a2d681753ebf1d23cc09
Eduardo Marques <edu.temarques@gmail.com>
2020-09-28 01:21:09 -0300

    fix: Adjusts in models

--- src/app/core/models/preparationStep.model.ts
+++ src/app/core/models/preparationStep.model.ts
@@ -1,4 +1,4 @@
 export interface PreparationStep {
   id?: number;
-  description?: number;
+  description?: string;
 }
--- src/app/core/models/system-option.model.ts
+++ src/app/core/models/system-option.model.ts
@@ -1,5 +1,4 @@
 export interface SystemOption {
-  id?: number;
-  systemValue?: string;
+  systemValue?: number;
   displayValue?: string;
 }
