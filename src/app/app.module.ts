0dd39818b1ea0491c9b534333e31bc2c2903a655
Eduardo Marques <edu.temarques@gmail.com>
2020-10-13 23:44:34 -0300

    feat: Adjusts in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
-	providers: [],
-	bootstrap: [AppComponent],
+  declarations: [AppComponent],
+  imports: [AppRoutingModule, BrowserModule, BrowserAnimationsModule, CoreModule],
+  providers: [],
+  bootstrap: [AppComponent],
+  exports: [],
 })
-export class AppModule {}
+export class AppModule { }
