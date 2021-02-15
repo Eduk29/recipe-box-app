a42d07d01c5b93d1926836bf9059e5300e737264
Eduardo Marques <edu.temarques@gmail.com>
2021-02-15 00:38:56 -0300

    feat: Add auto redirect to recipe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: 'recipe', loadChildren: () => import('./recipe/recipe.module').then(m => m.RecipeModule) }];
+const routes: Routes = [
+  { path: 'recipe', loadChildren: () => import('./recipe/recipe.module').then(m => m.RecipeModule) },
+  { path: '', redirectTo: 'recipe', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
